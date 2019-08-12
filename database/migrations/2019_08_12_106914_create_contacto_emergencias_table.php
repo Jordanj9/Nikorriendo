1b087b77133c62ec1aeaa39ad081bc6e3badc62e
Alberto Rojas <Alberto Rojas@DESKTOP-K4G9P0H>
2019-08-12 18:03:29 -0500

    version 2.6 12 de agosto de 2019 18:00 Alberto Rojas

---
+++ database/migrations/2019_08_12_106914_create_contacto_emergencias_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactoEmergenciasTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('contacto_emergencias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombres', 80);
+            $table->string('parentezco', 30);
+            $table->string('telefono', 11);
+            $table->string('email');
+            $table->string('direccion', 80);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('contacto_emergencias');
+    }
+
+}
