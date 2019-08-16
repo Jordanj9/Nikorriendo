4a4ebf9dfc6825fd85755f80c1cdb8d01ef20eb3
Jordan <Jordan@DESKTOP-B1L3QB8>
2019-08-16 15:31:35 -0500

    Version 6.3 16 de Agosto de 2019 15:30 JORDAN

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -8,15 +8,14 @@ use App\Sucursal;
 use App\User;
 use Illuminate\Http\Request;
 
-class PersonaController extends Controller
-{
+class PersonaController extends Controller {
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         $empleados = Persona::all();
         if (count($empleados) > 0) {
             foreach ($empleados as $item) {
@@ -24,8 +23,8 @@ class PersonaController extends Controller
             }
         }
         return view('estructura.empleado.list')
-            ->with('location', 'estructura')
-            ->with('empleados', $empleados);
+                        ->with('location', 'estructura')
+                        ->with('empleados', $empleados);
     }
 
     /**
@@ -33,12 +32,11 @@ class PersonaController extends Controller
      *
      * @return \Illuminate\Http\Responsesucursales
      */
-    public function create()
-    {
+    public function create() {
         $sucursales = Sucursal::all()->pluck('nombre', 'id');
         return view('estructura.empleado.create')
-            ->with('location', 'estructura')
-            ->with('sucursales', $sucursales);
+                        ->with('location', 'estructura')
+                        ->with('sucursales', $sucursales);
     }
 
     /**
@@ -47,24 +45,19 @@ class PersonaController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         $contacto = new Contacto_emergencia;
         $contacto->nombres = $request->get('nombre_contacto');
         $contacto->parentezco = $request->get('parentezco_contacto');
         $contacto->email = $request->get('email_contacto');
         $contacto->direccion = $request->get('direccion_contacto');
         $contacto->telefono = $request->get('telefono_contacto');
-
         foreach ($contacto->attributesToArray() as $key => $value) {
             $contacto->$key = strtoupper($value);
         }
-
         $result2 = $contacto->save();
-
         $persona = new Persona($request->all());
         $persona->contacto_emergencia_id = $contacto->id;
-
         foreach ($persona->attributesToArray() as $key => $value) {
             if ($key == 'email') {
                 $persona->$key = $value;
@@ -74,7 +67,7 @@ class PersonaController extends Controller
         }
         $result1 = $persona->save();
         if ($result1 && $result2) {
-            if(!User::where('identificacion', $persona->identificacion)){
+            if (User::where('identificacion', $persona->identificacion)->first() == null) {
                 $user = new User();
                 $user->identificacion = $persona->identificacion;
                 $user->nombres = $persona->primer_nombre . ' ' . $persona->segundo_nombre;
@@ -90,7 +83,6 @@ class PersonaController extends Controller
             flash("El empleado <strong>" . $persona->nombre . "</strong> no pudo ser almacenado(a). Error: ")->error();
             return redirect()->route('persona.index');
         }
-
     }
 
     /**
@@ -99,11 +91,10 @@ class PersonaController extends Controller
      * @param \App\Persona $persona
      * @return \Illuminate\Http\Response
      */
-    public function show(Persona $persona)
-    {
+    public function show(Persona $persona) {
         return view('estructura.empleado.show')
-            ->with('location', 'estructura')
-            ->with('persona', $persona);
+                        ->with('location', 'estructura')
+                        ->with('persona', $persona);
     }
 
     /**
@@ -112,13 +103,12 @@ class PersonaController extends Controller
      * @param \App\Persona $persona
      * @return \Illuminate\Http\Response
      */
-    public function edit(Persona $persona)
-    {
+    public function edit(Persona $persona) {
         $sucursales = Sucursal::all()->pluck('nombre', 'id');
         return view('estructura.empleado.edit')
-            ->with('location', 'estructura')
-            ->with('persona', $persona)
-           ->with('sucursales',$sucursales);
+                        ->with('location', 'estructura')
+                        ->with('persona', $persona)
+                        ->with('sucursales', $sucursales);
     }
 
     /**
@@ -128,23 +118,18 @@ class PersonaController extends Controller
      * @param \App\Persona $persona
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Persona $persona)
-    {
+    public function update(Request $request, Persona $persona) {
         $identificicacion = $persona->identificacion;
-
         $contacto = Contacto_emergencia::find($request->get('id_contacto'));
         $contacto->nombres = $request->get('nombre_contacto');
         $contacto->parentezco = $request->get('parentezco_contacto');
         $contacto->email = $request->get('email_contacto');
         $contacto->direccion = $request->get('direccion_contacto');
         $contacto->telefono = $request->get('telefono_contacto');
-
         foreach ($contacto->attributesToArray() as $key => $value) {
             $contacto->$key = strtoupper($value);
         }
-
         $result2 = $contacto->save();
-
         foreach ($persona->attributesToArray() as $key => $value) {
             if (isset($request->$key)) {
                 if ($key == 'email') {
@@ -154,9 +139,7 @@ class PersonaController extends Controller
                 $persona->$key = strtoupper($request->$key);
             }
         }
-
         $result1 = $persona->save();
-
         if ($result1 && $result2) {
             $user = User::where('identificacion', $identificicacion)->first();
             $user->identificacion = $persona->identificacion;
@@ -179,31 +162,28 @@ class PersonaController extends Controller
      * @param \App\Persona $persona
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Persona $persona)
-    {
+    public function destroy(Persona $persona) {
         $nombre = $persona->primer_nombre . ' ' . $persona->primer_apellido;
         $user = User::where('identificacion', $persona->identificacion)->first();
-
-        /*if(count($bodega->lavadoras) > 0){
-            return response()->json([
-                'status' => 'warning',
-                'message'=>"la bodega " .$nombre. " no pudo ser eliminado(a) porque tiene lavadoras asociadas. Error:"
-            ]);
-        }*/
-
+        /* if(count($bodega->lavadoras) > 0){
+          return response()->json([
+          'status' => 'warning',
+          'message'=>"la bodega " .$nombre. " no pudo ser eliminado(a) porque tiene lavadoras asociadas. Error:"
+          ]);
+          } */
         $result = $persona->delete();
         $result2 = $user->delete();
-
         if ($result && $result2) {
             return response()->json([
-                'status' => 'ok',
-                'message' => "el empleado " . $nombre . " fue eliminado(a) de forma exitosa!"
+                        'status' => 'ok',
+                        'message' => "el empleado " . $nombre . " fue eliminado(a) de forma exitosa!"
             ]);
         } else {
             return response()->json([
-                'status' => 'error',
-                'message' => "el empleado " . $nombre . " no pudo ser eliminado(a). Error:"
+                        'status' => 'error',
+                        'message' => "el empleado " . $nombre . " no pudo ser eliminado(a). Error:"
             ]);
         }
     }
+
 }
