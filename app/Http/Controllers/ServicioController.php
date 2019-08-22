26f37dd686ece7e7eea084462e055b72c70512bb
Jordan <Jordan@DESKTOP-B1L3QB8>
2019-08-21 19:59:22 -0500

    Version 8.5 21 de Agosto de 2019 20:00 Jordan

--- app/Http/Controllers/ServicioController.php
+++ app/Http/Controllers/ServicioController.php
@@ -43,6 +43,7 @@ class ServicioController extends Controller {
     public function create() {
         $u = Auth::user();
         $persona = Persona::where([['identificacion', $u->identificacion], ['tipo', 'CENTRAL']])->first();
+        $barrios = Barrio::all()->pluck('nombre', 'id');
         $existe = false;
         if ($persona != null) {
             $bodegas = Bodega::where('sucursal_id', $persona->sucursal_id)->get();
