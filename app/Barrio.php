f5f00cba0804cb78b500d39dbd7e67dfab4966bc
colonca <colonca1999@gmail.com>
2019-09-03 10:21:59 -0500

    Version 14.1 3 de septiembre de 2019 10:21 Camilo Colon

--- app/Barrio.php
+++ app/Barrio.php
@@ -4,9 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Barrio extends Model {
 
     /**
+     *
      * The attributes that are mass assignable.
      *
      * @var array
